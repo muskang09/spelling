ca4e5305e0af4473c0705b4cd5f7ab99d229059c
Muskan <muskangupta9566@gmail.com>
2024-06-12 14:49:07 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+// import logo from './logo.svg';
+// import './App.css';
+import SpellCheckApp from "./Spell";
+
+function App() {
+  return (
+    <div className="App">
+      <SpellCheckApp/>
+    </div>
+  );
+}
+
+export default App;
